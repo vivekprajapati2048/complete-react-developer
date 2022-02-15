78860443f6489943703038b99f919d468a4aec56
Vivek Prajapati <vivekprajapati2048@gmail.com>
2022-02-16 00:07:15 +0530

    028-dynamic content

--- monsters-rolodex/src/App.js
+++ monsters-rolodex/src/App.js
@@ -7,20 +7,30 @@ class App extends Component {
     super();
 
     this.state = {
-      string: "Hello Vivek",
+      monsters: [
+        {
+          name: 'Frankenstein',
+          id: 'asc1'
+        },
+        {
+          name: 'Dracula',
+          id: 'asc2'
+        },
+        {
+          name: 'Zombie',
+          id: 'asc3'
+        },
+      ]
     };
   }
 
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p> {this.state.string}</p>
-          <button onClick={() => this.setState({ string: "Hello User" })}>
-            Change Text
-          </button>
-        </header>
+       {this.state.monsters.map(
+           monster => <h1 key={monster.id}>{ monster.name }</h1>
+           )
+        }
       </div>
     );
   }
