8d43d5b12ba0a84db14fa46dd1f782706dfeaa68
Vivek Prajapati <vivekprajapati2048@gmail.com>
2022-02-15 21:51:37 +0530

    monsters rolodex

---
+++ monsters-rolodex/src/App.js
@@ -0,0 +1,54 @@
+import React, { Component } from "react";
+import logo from "./logo.svg";
+import "./App.css";
+
+class App extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      string: "Hello Vivek",
+    };
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p> {this.state.string}</p>
+          <button onClick={() => this.setState({ string: "Hello User" })}>
+            Change Text
+          </button>
+        </header>
+      </div>
+    );
+  }
+}
+
+export default App;
+
+//  Creating Functions instead of Class
+
+// function App() {
+//   return (
+//     <div className="App">
+//       <header className="App-header">
+//         <img src={logo} className="App-logo" alt="logo" />
+//         <p>
+//           Edit <code>src/App.js</code> and save to reload.
+//         </p>
+//         <a
+//           className="App-link"
+//           href="https://reactjs.org"
+//           target="_blank"
+//           rel="noopener noreferrer"
+//         >
+//           Learn React
+//         </a>
+//       </header>
+//     </div>
+//   );
+// }
+
+// export default App;
