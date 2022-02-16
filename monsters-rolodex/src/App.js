acdac85b30b87960214b4ed785ccc9d92c9de15e
Vivek Prajapati <vivekprajapati2048@gmail.com>
2022-02-16 19:38:32 +0530

    fetching content

--- monsters-rolodex/src/App.js
+++ monsters-rolodex/src/App.js
@@ -1,5 +1,4 @@
 import React, { Component } from "react";
-import logo from "./logo.svg";
 import "./App.css";
 
 class App extends Component {
@@ -7,30 +6,22 @@ class App extends Component {
     super();
 
     this.state = {
-      monsters: [
-        {
-          name: 'Frankenstein',
-          id: 'asc1'
-        },
-        {
-          name: 'Dracula',
-          id: 'asc2'
-        },
-        {
-          name: 'Zombie',
-          id: 'asc3'
-        },
-      ]
+      monsters: [],
     };
+  }
+
+  componentDidMount() {
+    fetch("https://jsonplaceholder.typicode.com/users")
+    .then(response => response.json())
+    .then(users => this.setState({ monsters: users }))
   }
 
   render() {
     return (
       <div className="App">
-       {this.state.monsters.map(
-           monster => <h1 key={monster.id}>{ monster.name }</h1>
-           )
-        }
+        {this.state.monsters.map((monster) => (
+          <h1 key={monster.id}>{monster.name}</h1>
+        ))}
       </div>
     );
   }
