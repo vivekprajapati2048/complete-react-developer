68f601e8dd410bbbf875ab9ff1dc7f9f2685c133
Vivek Prajapati <vivekprajapati2048@gmail.com>
2022-02-20 20:13:07 +0530

    searchfield state

--- monsters-rolodex/src/App.js
+++ monsters-rolodex/src/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from "react";
 import "./App.css";
-import { CardList } from './components/card-list/card-list.component';
+import { CardList } from "./components/card-list/card-list.component";
 
 class App extends Component {
   constructor() {
@@ -8,19 +8,25 @@ class App extends Component {
 
     this.state = {
       monsters: [],
+      searchField: "",
     };
   }
 
   componentDidMount() {
     fetch("https://jsonplaceholder.typicode.com/users")
-    .then(response => response.json())
-    .then(users => this.setState({ monsters: users }))
+      .then((response) => response.json())
+      .then((users) => this.setState({ monsters: users }));
   }
 
   render() {
     return (
       <div className="App">
-      <CardList monsters={this.state.monsters}/>
+        <input
+          type="search"
+          placeholder="search monster"
+          onChange={e => this.setState({ searchField: e.target.value })}
+        />
+        <CardList monsters={this.state.monsters} />
       </div>
     );
   }
